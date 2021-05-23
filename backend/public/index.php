6565cbb21600442112d5cf31144bef2683c1371d
Clofford <schwarz.clofford@gamil.com>
2021-05-23 14:03:55 +0900

    first commit

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
+/* hogehoge!!
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
