1e8ea36e5117e657477be33d1921798e9d186588
yoshitaka-tanaka0522 <yositana5227@gmail.com>
2021-05-30 19:20:01 +0900

    javasriptで削除処理追加

--- backend/resources/views/bulletin/show.blade.php
+++ backend/resources/views/bulletin/show.blade.php
@@ -20,14 +20,46 @@
          <p class="text-right mr-3">{{$bulletin -> account_name}} / {{ $bulletin -> question_id}}</p>
          <div class="row pt-2">
             <a href="/bulletin/edit/{{ $bulletin -> id }}" class="btn btn-primary col-1 ml-3 mr-2">編集する</a>
-            <a href="/bulletin/delete/{{ $bulletin -> id }}" class="btn btn-danger col-1">削除する</a>
+            <form method="POST" action="{{ route('bulletin.destroy',['id' => $bulletin->id]) }}" id="delete_{{ $bulletin->id }}">
+               @csrf
+               <a href="#" class="btn btn-danger" data-id="{{ $bulletin->id }}" onclick="deletePost(this);">削除する</a>
+            </form>
             <p class="col text-muted text-right mr-3">投稿日 {{$bulletin -> created_at}}</p>
          </div>
       </div>
    </div>
+   <div class="row">
+      @foreach($comments as $comment)
+      <div class="offset-md-5 col-md-5">
+          <p class="h3">{{$comment->content}}</p>
+          <label>{{$comment->created_at}}</label>
+      </div>
+      @endforeach
+  </div>
+
+  @auth
+  <div class="row">
+      <div class="offset-md-5 col-md-5">
+          <form method="POST" action="/{{ $bulletin->id }}/comments">
+              {{ csrf_field() }}
+              <textarea name="content" class="form-control m-2"></textarea>
+              <button type="submit" class="btn samazon-submit-button ml-2">レビューを追加</button>
+          </form>
+      </div>
+  </div>
+  @endauth                
+</div>   
    <div>
       <a href="/bulletin" class="ml-1">back</a>
    </div>
 </div>
+<script>
+   function deletePost(elem) {
+       'use strict';
+       if(confirm('本当に削除しても良いですか？')) {
+           document.getElementById('delete_'+elem.dataset.id).submit();
+       }
+   }
+</script>
 </body>
 </html>
